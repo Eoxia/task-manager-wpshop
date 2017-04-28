2e00df975f4817dd73d6d5d2cc17cc009dc7b909
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-04-28 16:40:33 +0200

    maj

--- modules/support/view/frontend/main.view.php
+++ modules/support/view/frontend/main.view.php
@@ -13,13 +13,13 @@
 if ( ! defined( 'ABSPATH' ) ) { exit; } ?>
 
 <div class="wpeo-window-ask-task-container">
-	<a href="#" class="wpeo-ask-task"><?php _e( 'Ask a ticket', 'task-manager' ); ?></a>
+	<a href="#" class="wpeo-ask-task"><?php esc_html_e( 'Ask a ticket', 'task-manager' ); ?></a>
 
 	<div id="wpeo-window-ask-task" >
-		<form action="<?php echo admin_url( 'admin-ajax.php' ); ?>" method="POST">
+		<form class="form" action="<?php echo esc_attr( admin_url( 'admin-ajax.php' ) ); ?>" method="POST">
 			<input type="hidden" name="action" value="ask_task">
-			<input type="text" name="point[content]" placeholder="<?php _e( 'Write your ticket', 'task-manager' ); ?>">
-			<input type="button" value="<?php _e( 'Confirm', 'task-manager' ); ?>">
+			<input type="text" name="point[content]" placeholder="<?php esc_html_e( 'Write your ticket', 'task-manager' ); ?>">
+			<input type="button" class="action-input" data-parent="form" data-nonce="<?php echo esc_attr( wp_create_nonce( 'ask_task' ) ); ?>" value="<?php esc_html_e( 'Confirm', 'task-manager' ); ?>">
 		</form>
 	</div>
 </div>
