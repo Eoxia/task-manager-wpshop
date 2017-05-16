f6a6bc5256b23977b3f926b659be278b88bdbc10
amandine-eoxia <amandine@eoxia.com>
2017-05-16 16:28:51 +0200

    MAJ template nouveau commentaire

--- modules/support/view/frontend/comment/edit.view.php
+++ modules/support/view/frontend/comment/edit.view.php
@@ -14,25 +14,32 @@ namespace task_manager_wpshop;
 
 if ( ! defined( 'ABSPATH' ) ) { exit; } ?>
 
-<li class="comment edit">
-	<input type="hidden" name="action" value="edit_comment" />
-	<?php wp_nonce_field( 'edit_comment' ); ?>
-	<input type="hidden" name="comment_id" value="<?php echo esc_attr( $comment->id ); ?>" />
-	<input type="hidden" name="post_id" value="<?php echo esc_attr( $task_id ); ?>" />
-	<input type="hidden" name="parent_id" value="<?php echo esc_attr( $point_id ); ?>" />
+<li class="comment new">
+	<form action="<?php echo esc_attr( admin_url( 'admin-ajax.php' ) ); ?>" method="POST">
+		<ul class="wpeo-comment-container">
 
-	<ul class="wpeo-comment-container">
-		<li class="avatar"><?php echo do_shortcode( '[task_avatar ids="' . $comment->author_id . '" size="16"]' ); ?></li>
-		<li class="wpeo-comment-date"><span class="fa fa-calendar-o"></span> <input type="text" class="date" name="date" value="<?php echo esc_attr( $comment->date ); ?>" /></li>
-		<li class="wpeo-save-point"><i data-parent="comment" class="action-input fa fa-floppy-o" aria-hidden="true"></i></li>
-		<li class="wpeo-comment-content">
-			<input type="hidden" name="content" value="<?php esc_attr( $comment->content ); ?>" />
-			<div class="content" contenteditable="true">
-				<?php echo $comment->content; ?>
-			</div>
-			<?php if ( empty( $comment->id ) ) : ?>
-				<span class="wpeo-point-new-placeholder"><?php esc_html_e( 'Votre commentaire ici...', 'task-manager' ); ?></span>
+			<input type="hidden" name="action" value="edit_comment" />
+			<?php wp_nonce_field( 'edit_comment' ); ?>
+			<input type="hidden" name="comment_id" value="<?php echo esc_attr( $comment->id ); ?>" />
+			<input type="hidden" name="post_id" value="<?php echo esc_attr( $task_id ); ?>" />
+			<input type="hidden" name="parent_id" value="<?php echo esc_attr( $point_id ); ?>" />
+
+			<li class="wpeo-comment-content">
+				<input type="hidden" name="content" value="<?php esc_attr( $comment->content ); ?>" />
+				<div class="content" contenteditable="true">
+					<?php echo $comment->content; ?>
+				</div>
+				<?php if ( empty( $comment->id ) ) : ?>
+					<span class="wpeo-point-new-placeholder"><?php esc_html_e( 'Votre commentaire ici...', 'task-manager' ); ?></span>
+				<?php endif; ?>
+			</li>
+
+			<?php if ( ! empty( $comment->id ) ) : ?>
+				<li class="wpeo-save-point"><i data-parent="edit" class="action-input fa fa-floppy-o" aria-hidden="true"></i></li>
+			<?php else : ?>
+				<span data-parent="edit" class="wpeo-point-new-btn action-input dashicons dashicons-plus-alt"></span>
 			<?php endif; ?>
-		</li>
-	</ul>
+
+		</ul>
+	</form>
 </li>
