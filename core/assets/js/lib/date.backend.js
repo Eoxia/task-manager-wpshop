8a474c46a7f2d7d074f33dc8b3df9433fffd8a33
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-05-31 11:18:23 +0200

    Déplaçement vers task manager

--- core/assets/js/lib/date.backend.js
+++ core/assets/js/lib/date.backend.js
@@ -1,11 +1,23 @@
-window.taskManagerWpshop.date = {};
+if ( ! window.eoxiaJS.date ) {
 
-window.taskManagerWpshop.date.init = function() {
-	jQuery( document ).on( 'click', '.date', function( e ) {
-		jQuery( this ).datepicker( {
-			dateFormat: 'dd/mm/yy'
+	window.eoxiaJS.date = {};
+
+	window.eoxiaJS.date.init = function() {
+		jQuery( document ).on( 'click', 'input.date', function( e ) {
+			jQuery( this ).datetimepicker( {
+				'lang': 'fr',
+				'format': 'd/m/Y',
+				timepicker: false
+			} );
+			jQuery( this ).datetimepicker( 'show' );
 		} );
 
-		jQuery( this ).datepicker( 'show' );
-	} );
-};
+		jQuery( document ).on( 'click', 'input.date-time', function( e ) {
+			jQuery( this ).datetimepicker( {
+				'lang': 'fr',
+				'format': 'd/m/Y H:i'
+			} );
+			jQuery( this ).datetimepicker( 'show' );
+		} );
+	};
+}
--- modules/support/asset/js/support.frontend.js
+++ modules/support/asset/js/support.frontend.js
@@ -1,32 +1,27 @@
-window.taskManagerWpshop.frontendSupport = {};
+window.eoxiaJS.taskManagerWpshop.frontendSupport = {};
 
-window.taskManagerWpshop.frontendSupport.init = function() {
-	window.taskManagerWpshop.frontendSupport.event();
+window.eoxiaJS.taskManagerWpshop.frontendSupport.init = function() {
+	window.eoxiaJS.taskManagerWpshop.frontendSupport.event();
 };
 
-window.taskManagerWpshop.frontendSupport.event = function() {
-	jQuery( document ).on( 'click', '.wpeo-ask-task', window.taskManagerWpshop.frontendSupport.slideAskTask );
-	jQuery( document ).on( 'keyup', '.wps-section-content .task-search', window.taskManagerWpshop.frontendSupport.searchKey );
-	jQuery( document ).on( 'click', '.wps-section-content .search-button', window.taskManagerWpshop.frontendSupport.searchIn );
-
-	jQuery( document ).on( 'click', '.wpeo-task-point-use-toggle', window.taskManagerWpshop.frontendSupport.togglePoint );
-	jQuery( document ).on( 'click', '.point-content', window.taskManagerWpshop.frontendSupport.setPointActive );
-
-jQuery( document ).on( 'blur keyup paste keydown click', '.comment .content', window.taskManagerWpshop.frontendSupport.updateHiddenInput );
+window.eoxiaJS.taskManagerWpshop.frontendSupport.event = function() {
+	jQuery( document ).on( 'click', '.wpeo-ask-task', window.eoxiaJS.taskManagerWpshop.frontendSupport.slideAskTask );
+	jQuery( document ).on( 'keyup', '.wps-section-content .task-search', window.eoxiaJS.taskManagerWpshop.frontendSupport.searchKey );
+	jQuery( document ).on( 'click', '.wps-section-content .search-button', window.eoxiaJS.taskManagerWpshop.frontendSupport.searchIn );
 };
 
-window.taskManagerWpshop.frontendSupport.slideAskTask = function( event ) {
+window.eoxiaJS.taskManagerWpshop.frontendSupport.slideAskTask = function( event ) {
 	event.preventDefault();
 	jQuery( '#wpeo-window-ask-task' ).slideToggle();
 };
 
-window.taskManagerWpshop.frontendSupport.searchKey = function( event ) {
+window.eoxiaJS.taskManagerWpshop.frontendSupport.searchKey = function( event ) {
 	if ( 13 === event.keyCode ) {
 		jQuery( '.wps-section-content .search-button' ).click();
 	}
 };
 
-window.taskManagerWpshop.frontendSupport.searchIn = function( event ) {
+window.eoxiaJS.taskManagerWpshop.frontendSupport.searchIn = function( event ) {
 	var element = jQuery( this );
 	if ( 0 == jQuery( this ).closest( '.wps-section-content' ).find( '.task-search' ).val().length ) {
 		jQuery( '.list-task .wpeo-project-task' ).show();
@@ -46,74 +41,10 @@ window.taskManagerWpshop.frontendSupport.searchIn = function( event ) {
 	}
 };
 
-window.taskManagerWpshop.frontendSupport.togglePoint = function( event ) {
-	event.preventDefault();
-	jQuery( this ).find( '.wpeo-point-toggle-arrow' ).toggleClass( 'dashicons-plus dashicons-minus' );
-	jQuery( this ).closest( '.wpeo-task-point-use-toggle' ).find( '.points.completed' ).toggleClass( 'hidden' );
-};
-
-window.taskManagerWpshop.frontendSupport.setPointActive = function( event ) {
-	jQuery( '.point-content.active' ).removeClass( 'active' );
-	jQuery( this ).addClass( 'active' );
-};
-
-/**
- * Avant de charger les commentaires, change la dashicons.
- *
- * @param  {HTMLSpanElement} triggeredElement L'élément HTML déclenchant l'action.
- * @return void
- *
- * @since 1.3.6.0
- * @version 1.3.6.0
- */
-window.taskManagerWpshop.frontendSupport.beforeLoadComments = function( triggeredElement ) {
-	triggeredElement.toggleClass( 'dashicons-arrow-right-alt2 dashicons-arrow-down-alt2' );
-
-	if ( triggeredElement.hasClass( 'dashicons-arrow-right-alt2' ) ) {
-		triggeredElement.closest( 'div.point' ).find( '.comments' ).toggleClass( 'hidden' );
-		return false;
-	}
-
-	return true;
-};
-
-window.taskManagerWpshop.frontendSupport.loadedFrontComments = function( triggeredElement, response ) {
-	jQuery( triggeredElement ).closest( '.point' ).find( '.comments' ).html( response.data.view );
-	triggeredElement.closest( 'div.point' ).find( '.comments' ).toggleClass( 'hidden' );
-};
-
-window.taskManagerWpshop.frontendSupport.addedCommentSuccess = function( triggeredElement, response ) {
-	jQuery( triggeredElement ).closest( '.wpeo-comment-container' ).find( 'input[name="content"]' ).val( '' );
-	jQuery( triggeredElement ).closest( '.wpeo-comment-container' ).find( '.content' ).html( '' );
-	jQuery( triggeredElement ).closest( '.comments .comment.new' ).after( response.data.view );
-	jQuery( triggeredElement ).closest( '.comment' ).find( '.wpeo-point-new-placeholder' ).removeClass( 'hidden' );
-};
-
-window.taskManagerWpshop.frontendSupport.askedTask = function( triggeredElement, response ) {
+window.eoxiaJS.taskManagerWpshop.frontendSupport.askedTask = function( triggeredElement, response ) {
 	if ( response.data.edit ) {
 		jQuery( '.wpeo-project-task[data-id="' + response.data.task_id + '"]' ).replaceWith( response.data.template );
 	} else {
 		jQuery( '.list-task' ).prepend( response.data.template );
 	}
-};
-
-/**
- * Met à jour le champ caché contenant le texte du comment écris dans la div "contenteditable".
- *
- * @param  {MouseEvent} event L'évènement de la souris lors de l'action.
- * @return {void}
- *
- * @since 1.0.0.0
- * @version 1.3.6.0
- */
-window.taskManagerWpshop.frontendSupport.updateHiddenInput = function( event ) {
-	if ( 0 < jQuery( this ).text().length ) {
-		jQuery( this ).closest( '.comment' ).find( '.wpeo-point-new-btn' ).css( 'opacity', 1 );
-		jQuery( this ).closest( '.comment' ).find( '.wpeo-point-new-placeholder' ).addClass( 'hidden' );
-	} else {
-		jQuery( this ).closest( '.comment' ).find( '.wpeo-point-new-btn' ).css( 'opacity', 0.4 );
-		jQuery( this ).closest( '.comment' ).find( '.wpeo-point-new-placeholder' ).removeClass( 'hidden' );
-	}
-
-	jQuery( this ).closest( '.comment' ).find( '.wpeo-comment-content input[name="content"]' ).val( jQuery( this ).html() );
 };
