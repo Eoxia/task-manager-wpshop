ea91363fb7fbf4bf5652df564ce628298a784c0b
Alexandre Techer - Eoxia <alexandre.techer@eoxia.com>
2017-10-13 11:09:50 +0200

    1.2.0: Fix problème affichage support client

--- modules/support/view/frontend/main.view.php
+++ modules/support/view/frontend/main.view.php
@@ -9,16 +9,17 @@
  * @package Task_Manager_WPShop
  */
 
+namespace task_manager_wpshop;
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 } ?>
-
 <div class="wpeo-project-wrap">
 	<div class="wpeo-window-ask-task-container">
 		<a href="#" class="wpeo-ask-task"><?php esc_html_e( 'Ask a ticket', 'task-manager-wpshop' ); ?></a>
 
 		<div id="wpeo-window-ask-task" >
-			<form class="form" action="<?php echo esc_attr( admin_url( 'admin-ajax.php' ) ); ?>" method="POST">
+			<form class="form" action="<?php echo esc_attr( admin_url( 'admin-ajax.php' ) ); ?>" method="POST" >
 				<input type="hidden" name="action" value="ask_task">
 				<input type="text" name="point[content]" placeholder="<?php esc_html_e( 'Write your ticket', 'task-manager-wpshop' ); ?>">
 				<input type="button" data-loader="form" class="action-input" data-parent="form" data-nonce="<?php echo esc_attr( wp_create_nonce( 'ask_task' ) ); ?>" value="<?php esc_html_e( 'Confirm', 'task-manager-wpshop' ); ?>">
@@ -27,7 +28,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	</div>
 
 	<div class="wpeo-project-search">
-		<input type="text" class="task-search" placeholder="Recherche..">
+		<input type="text" class="task-search" placeholder="<?php esc_attr_e( 'Search...', 'task-manager-wpshop' ); ?>">
 		<button class="search-button"><span class="dashicons dashicons-search"></span></button>
 	</div>
 </div>
@@ -36,9 +37,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <?php
 $posts = get_posts( array(
-	'author' => get_current_user_id(),
-	'post_type' => 'wpshop_shop_order',
-	'post_status' => 'any',
+	'post_parent'  => $parent_id,
+	'post_type'    => 'wpshop_shop_order',
+	'post_status'  => 'any',
 ) );
 if ( ! empty( $posts ) ) :
 	foreach ( $posts as $post ) :
