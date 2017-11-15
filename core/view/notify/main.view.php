1f1be54ee0afd2e4a783f8353be15a2f2c91307c
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-11-15 10:29:13 +0100

    1.2.0: Affichages des informations du client

--- core/view/notify/main.view.php
+++ core/view/notify/main.view.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Ajout de la checkbox ? pour confirmer la notification au client.
+ * Ajout de la checkbox pour confirmer la notification au client.
  *
  * @author Jimmy Latour <jimmy.eoxia@gmail.com>
  * @since 1.2.0
@@ -18,4 +18,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 <div class="notify-label">
 	<label for="notify_customer"><?php echo esc_html_e( 'Notify customer ?', 'task-manager-wpshop' ); ?></label>
 	<input id="notify_customer" name="notify_customer" type="checkbox" />
+
+	<div class="more-info hidden">
+
+		<p>
+			<?php
+			// translators: You're going make a notify to the customer technique@eoxia.com.
+			echo sprintf( __( 'You\'re gonna to send a notification to the customer %s', 'task-manager-wpshop' ), $user_info->user_email );
+			?>
+		</p>
+
+		<p>
+			<?php
+			// translators: Mail subject: Nouve message sur votre support.
+			echo sprintf( __( 'Mail subject: %s', 'task-manager-wpshop' ), $post->post_title );
+			?>
+		</p>
+	</div>
+
+
 </div>
