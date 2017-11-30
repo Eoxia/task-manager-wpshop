751ff281407255800d71e2e0836e53804988a3a3
Amandine Eoxia <amandine@eoxia.com>
2017-11-30 14:38:37 +0100

    1.2.0: Modification template indicators

--- module/indicator/view/backend/request.view.php
+++ module/indicator/view/backend/request.view.php
@@ -9,66 +9,70 @@
  * @package Task_Manager_WPShop
  */
 
+namespace task_manager_wpshop;
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 } ?>
 
-<?php if ( ! empty( $comments ) ) : ?>
-	<ul class="tm-wps-customer-requests">
-		<?php
-		foreach ( $comments as $comment ) :
-			?>
-			<li>
-				<span><?php echo esc_html( $comment->date['date_human_readable'] ); ?></span>
-			</li>
+<div class="daily-activity activities">
+	<div class="content">
+		<?php if ( ! empty( $comments ) ) : ?>
+			<?php foreach ( $comments as $comment ) : ?>
 
-			<?php if ( null !== $comment->post_parent ) : ?>
-				<li class="tmp-wps-customer-requests-customer">
-					<span>
-						<?php esc_html_e( 'On the customer', 'task-manager-wpshop' ); ?>
-						<a href="<?php echo esc_url( admin_url( 'post.php?post=' . $comment->post_parent->ID . '&action=edit' ) ); ?>">
-							#<?php echo esc_html( $comment->post_parent->ID ); ?>
-							 - <?php echo esc_html( $comment->post_parent->post_title ); ?>
-						</a>
-					</span>
-				</li>
-			<?php endif; ?>
-			<li>
-				<span>
-					<?php esc_html_e( 'On the task', 'task-manager-wpshop' ); ?>
-					<a href="<?php echo esc_url( admin_url( 'post.php?post=' . $comment->post_parent->ID . '&term=' . $comment->task->id . '&action=edit' ) ); ?>">
-						#<?php echo esc_html( $comment->task->id ); ?>
-						- <?php echo esc_html( $comment->task->title ); ?>
-					</a>
-					</span>
-				</li>
-				<li>
-					<span>
-						<?php esc_html_e( 'On the point', 'task-manager-wpshop' ); ?>
-						<a href="<?php echo esc_url( admin_url( 'post.php?post=' . $comment->post_parent->ID . '&term=' . $comment->task->id . '&action=edit&point_id=' . $comment->point->id ) ); ?>">
-							#<?php echo esc_html( $comment->point->id ); ?>
-							- <?php echo esc_html( $comment->point->content ); ?>
-						</a>
-					</span>
-				</li>
-				<li>
-					<span>
-						<?php echo esc_html_e( 'With the comment:', 'task-manager-wpshop' ); ?>
-						<a href="<?php echo esc_url( admin_url( 'post.php?post=' . $comment->post_parent->ID . '&term=' . $comment->task->id . '&action=edit&point_id=' . $comment->point->id . '&comment_id=' . $comment->id ) ); ?>">
-							#<?php echo esc_html( $comment->id ); ?>
-							- <?php
-							echo wp_kses( $comment->content, array(
-								'br' => array(),
-								'p' => array(),
-							) );
+				<div class="activity">
+					<div class="information">
+						<?php echo do_shortcode( '[task_avatar ids="' . $comment->author_id . '" size="30"]' ); ?>
+						<span class="time-posted"><?php echo esc_html( mysql2date( 'H\hi', $comment->date['date_input']['date'], true ) ); ?></span>
+					</div>
+
+					<div class="content">
+						<div class="event-header">
+							<!-- Client -->
+								<span class="event-client">
+									<i class="fa fa-user"></i>
+									<?php if ( ! empty( $comment->post_parent->ID ) ) : ?>
+									<a href="<?php echo esc_url( get_permalink( $comment->post_parent->ID ) ); ?>" target="wptm_view_activity_element" >
+										<?php echo esc_html( '#' . $comment->post_parent->ID . ' ' . $comment->post_parent->post_title ); ?>
+									</a>
+								<?php else : ?>
+									<?php echo esc_html( '-' ); ?>
+								<?php endif; ?>
+								</span>
+							<!-- Tâche -->
+							<span class="event-task">
+								<i class="dashicons dashicons-layout"></i> <?php echo esc_html( '#' . $comment->task->id . ' ' . $comment->task->title ); ?>
+							</span>
+							<!-- Point -->
+							<span class="event-point">
+								<i class="fa fa-list-ul"></i> <?php echo esc_html( '#' . $comment->point->id . ' ' . $comment->point->content ); ?>
+							</span>
+						</div>
+
+						<span class="event-content">
+
+
+							<?php
+							$link = 'admin.php?page=wpeomtm-dashboard&term=' . $comment->task->id . '&point_id=' . $comment->point->id . '&comment_id=' . $comment->id;
+							if ( ! empty( $comment->post_parent->id ) ) :
+								$link = 'post.php?post=' . $comment->post_parent->id . '&term=' . $comment->task->id . '&action=edit&point_id=' . $comment->point->id . '&comment_id=' . $comment->id;
+							endif;
 							?>
-						</a>
-					</span>
-				</li>
-			<?php
-		endforeach;
-		?>
-	</ul>
-<?php else : ?>
-	<?php esc_html_e( 'No customer waiting request for the moment', 'task-manager-wpshop' ); ?>
-<?php endif; ?>
+							<a target="wptm_view_activity_element" href="<?php echo esc_url( admin_url( $link ) ); ?>" >
+								<?php
+								echo wp_kses( $comment->content, array(
+									'br' => array(),
+									'p' => array(),
+								) );
+								?>
+							</a>
+						</span>
+					</div>
+				</div>
+
+			<?php endforeach; ?>
+		<?php else : ?>
+			<?php esc_html_e( 'No pending requests', 'task-manager-wpshop' ); ?>
+		<?php endif; ?>
+	</div><!-- .content -->
+</div>
